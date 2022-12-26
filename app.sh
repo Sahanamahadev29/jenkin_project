aceb2921f1c35be2e46469938fd50b8dfb1cdcc1
Sahanamahadev29 <113870446+Sahanamahadev29@users.noreply.github.com>
2022-12-26 15:11:23 +0530

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,4 +1,22 @@
-#!/bin/sh
-sudo apt-get update
-sudo apt-get install netcat -y
-sudo apt install awscli -y
+#!/bin/bash
+install_aws_cli()
+{
+    sudo apt-get update
+    sudo apt-get -y install awscli 
+    if [ $? -ne 0 ]; then
+       echo "error while installing awscli"
+    fi
+    echo "awscli installed successfully"
+}
+install_netcat()
+{
+    sudo apt-get update
+    sudo apt-get -y install netcat-traditional
+    if [ $? -ne 0 ]; then
+       echo "error while installing netcat"
+    fi
+    echo "netcat installed successfully"
+}
+install_aws_cli
+aws --version
+install_netcat
